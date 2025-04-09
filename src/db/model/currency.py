1075b450beae3103b045de4be2ee6ee6e110c8e0
TheEmpressDiadema <mauhkpaffftep@gmail.com>
2025-04-09 16:35:19 +0300

    feat: currency model

---
+++ src/db/model/currency.py
@@ -0,0 +1,20 @@
+from dataclasses import dataclass
+
+@dataclass
+class Currency:
+    id: int
+    code: str
+    full_name: str
+    sign: str
+    created: str
+    updated: str
+
+    def __str__(self):
+        return str({
+            "id": self.id,
+            "code": self.code,
+            "full_name": self.full_name,
+            "sign": self.sign,
+            "created": self.created,
+            "updated": self.updated
+        })
