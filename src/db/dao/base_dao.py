6e8841d368d66bb9d99dacc047dc9b6d117d562a
TheEmpressDiadema <mauhkpaffftep@gmail.com>
2025-04-10 16:49:35 +0300

    feat: base dao class implementation

--- src/db/dao/base_dao.py
+++ src/db/dao/base_dao.py
@@ -1,10 +1,45 @@
 import sqlite3
 
+
 class BaseDao:
 
-    db_folder: str = "src\db\data"
-    db_url: str
-    db_name: str
+    def __init__(self, db_path: str):
+        self._db_path = db_path
 
-    def init(self, db_name: str):
-        self.db_name = db_name
+    def create_connection(self) -> sqlite3.Connection:
+
+        try:
+
+            return sqlite3.connect(self._db_path)
+        
+        except Exception as exception:
+
+            print(f"Can't create connection with {self.db_path}", exception)
+    
+    def _get_generated_id(self, table_name: str) -> int:
+        
+        try:
+
+            with self.create_connection() as connection:
+                cursor = connection.cursor()
+                cursor.execute(f"SELECT MAX(id) as max_id FROM {table_name}")
+                result = cursor.fetchone()
+                return result[0]
+        
+        except Exception as error_msg:
+
+            print(f"Can't get max(id) value from {table_name}", error_msg)
+
+    def execute_changes_query(self, query_string: str, values: tuple[any], exception_string: str):
+
+        try: 
+
+            with self.create_connection() as connection:
+
+                cursor = connection.cursor()
+                cursor.execute(query_string, values)
+                connection.commit()
+        
+        except Exception as error_msg:
+
+            print(exception_string, error_msg)
