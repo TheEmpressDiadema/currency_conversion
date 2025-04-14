8fa4c7e483785ef934f7f42ca2f7acde90a08d46
TheEmpressDiadema <mauhkpaffftep@gmail.com>
2025-04-14 16:57:53 +0300

    refactor: removed generate_id method, logging error replaces print, removed execute changes query method

--- src/db/dao/base_dao.py
+++ src/db/dao/base_dao.py
@@ -1,45 +1,14 @@
 import sqlite3
-
+import logging
 
 class BaseDao:
-
     def __init__(self, db_path: str):
         self._db_path = db_path
 
-    def create_connection(self) -> sqlite3.Connection:
-
+    def _get_connection(self) -> sqlite3.Connection:
+        
         try:
-
             return sqlite3.connect(self._db_path)
         
-        except Exception as exception:
-
-            print(f"Can't create connection with {self.db_path}", exception)
-    
-    def _get_generated_id(self, table_name: str) -> int:
-        
-        try:
-
-            with self.create_connection() as connection:
-                cursor = connection.cursor()
-                cursor.execute(f"SELECT MAX(id) as max_id FROM {table_name}")
-                result = cursor.fetchone()
-                return result[0]
-        
-        except Exception as error_msg:
-
-            print(f"Can't get max(id) value from {table_name}", error_msg)
-
-    def execute_changes_query(self, query_string: str, values: tuple[any], exception_string: str):
-
-        try: 
-
-            with self.create_connection() as connection:
-
-                cursor = connection.cursor()
-                cursor.execute(query_string, values)
-                connection.commit()
-        
-        except Exception as error_msg:
-
-            print(exception_string, error_msg)
+        except Exception as connect_error:
+            logging.error(f"Can't connect {self._db_path}", connect_error)
