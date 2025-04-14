c8fc4dc2482b3382e34ba452354b9288a8c02775
TheEmpressDiadema <mauhkpaffftep@gmail.com>
2025-04-14 16:58:16 +0300

    feat: test values changed

--- t.py
+++ t.py
@@ -1,25 +1,9 @@
-from dataclasses import dataclass
-from datetime import datetime
+import datetime
+from src.db.model.currency import Currency
+from src.db.dao.currency_dao import CurrencyDao
 
-@dataclass
-class Currency:
-    id: int
-    code: str
-    full_name: str
-    sign: str
-    created: str
-    updated: str
 
-    def __str__(self):
-        return str({
-            "id" : self.id,
-            "code": self.code,
-            "full_name": self.full_name,
-            "sign": self.sign,
-            "created": self.created,
-            "updated": self.updated
-        })
-
-if __name__ == "__main__":
-    curr = Currency(1, "USD", "United States Dollar", "$", str(datetime.now()), str(datetime.now()))
-    print(curr)
+curr_time = str(datetime.datetime.today()).split()[0]
+currency = Currency(1, "EUR", "EURO", "Э", curr_time, curr_time)
+dao = CurrencyDao(r"src\db\data\data.db")
+dao.update(currency)
