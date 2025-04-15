c96cca0cf5505903477021ac9d386d730e69d875
TheEmpressDiadema <mauhkpaffftep@gmail.com>
2025-04-15 16:49:51 +0300

    feat: some dao test

--- t.py
+++ t.py
@@ -6,4 +6,4 @@ from src.db.dao.currency_dao import CurrencyDao
 curr_time = str(datetime.datetime.today()).split()[0]
 currency = Currency(1, "EUR", "EURO", "Э", curr_time, curr_time)
 dao = CurrencyDao(r"src\db\data\data.db")
-dao.update(currency)
+print(dao.get_all())
