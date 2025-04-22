4cc57c39883c002fd042c8dd46e21eab8d2ad9dc
TheEmpressDiadema <mauhkpaffftep@gmail.com>
2025-04-22 16:57:51 +0300

    feat: controller classes to implement, get_dto_list method

--- src/web/controller/currency_controller.py
+++ src/web/controller/currency_controller.py
@@ -1,6 +1,10 @@
 from src.db.dao.currency_dao import CurrencyDao
-
+from src.web.dto.currency_dto import CurrencyDto
 
 class CurrencyController:
 
     dao: CurrencyDao = CurrencyDao()
+
+    def get_dto_list(self) -> list[CurrencyDto]:
+        dto_list = self.dao.get_all()
+        return [CurrencyDto.from_model(dto) for dto in dto_list]
--- src/web/controller/exchange_rate_controller.py
+++ src/web/controller/exchange_rate_controller.py
@@ -1,5 +1,10 @@
 from src.db.dao.exchange_rate_dao import ExchangeRateDao
+from src.web.dto.exchange_rate_dto import ExchangeRateDto
 
 class ExchangeRateController:
 
     dao: ExchangeRateDao = ExchangeRateDao()
+
+    def get_dto_list(self) -> list[ExchangeRateDto]:
+        dto_list = self.dao.get_all()
+        return [ExchangeRateDto.from_model(dto) for dto in dto_list]
