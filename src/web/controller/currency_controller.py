cd6f7bc80686b9a21349b98fd5eb0db7ea0f4014
TheEmpressDiadema <mauhkpaffftep@gmail.com>
2025-04-22 16:16:39 +0300

    feat: controller classes are added to implement

---
+++ src/web/controller/currency_controller.py
@@ -0,0 +1,6 @@
+from src.db.dao.currency_dao import CurrencyDao
+
+
+class CurrencyController:
+
+    dao: CurrencyDao = CurrencyDao()
---
+++ src/web/controller/exchange_rate_controller.py
@@ -0,0 +1,5 @@
+from src.db.dao.exchange_rate_dao import ExchangeRateDao
+
+class ExchangeRateController:
+
+    dao: ExchangeRateDao = ExchangeRateDao()
